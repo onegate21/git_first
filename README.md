e8a471e09df409e702e68c89a5cc3801a03fff31
onegate21 <80009344+onegate21@users.noreply.github.com>
2021-03-05 13:30:34 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
+
 # git_first
+
+# 주석처리 Tip : Ctrl + '/'
+
+# CSS 선택자 우선순위 :  TAG ( ) < ClASS ( . ) <  ID ( # )
+  ex) <li><a href = "1.html" class="saw" id="active">HTML</a></li>
+
+
+#【배열 _ Array】
+  var coworkers = [
+      "egoing", 
+      "leezche"
+      ]
+
+
+#【객체 _ Object】_ JavaScript 32강
+  var coworkers = {
+      "programmer" : "egoing", 
+      "designer"   : "leezche"
+      }
+
+  - document.write("designer : " + coworkers.designer);     // 가져오는 법
+  - coworkers.bookkeeper        = "dure";                   // 입력하는 법
+  - coworkers["data scientist"] = "taeho";  
+
+    <script>
+        for(var key in coworkers) {                         // iterate (반복하다. 되풀이하다.)
+            document.write(key + ' : ' + coworkers[key]+ '<br>');
+        }
+    </script>
+
+    <script>
+        coworkers.showAll = function() {
+        }
+
+        function showAll() {            
+        }
+    </script>
+
+# Library vs framework 
+    - Library : jQuery 
+
+
+
+
+
+
+
+
+
+
+
---
+++ color.js
@@ -0,0 +1,41 @@
+
+
+
+var Link = {
+    SetColor : function(iColor) {        
+        // var links = document.querySelectorAll('a');
+        // var i = 0;
+        // while(i < links.length) {
+        //     links[i].style.color=iColor;
+        //     i=i+1;
+        // }
+        $('a').css('color', iColor);    //  jQuery 활용 예
+    }
+}
+
+var Body = {
+    SetBackgroundColor : function(iColor) {
+        //document.querySelector('body').style.backgroundColor = iColor;
+        $('body').css('backgroundColor', iColor);    //  jQuery 활용 예
+    },            
+    SetColor : function(iColor) {
+        //document.querySelector('body').style.color = iColor;
+        $('body').css('color', iColor);    //  jQuery 활용 예
+    }           
+} 
+
+function nightdayHandler(self){
+    var target = document.querySelector('body');
+
+    if (self.value === 'night') {
+        Body.SetBackgroundColor('black');
+        Body.SetColor('white');
+        self.value = 'day';
+        Link.SetColor('yellow');
+    } else {
+        Body.SetBackgroundColor('white');                    
+        Body.SetColor('black');
+        self.value = 'night';
+        Link.SetColor('blue');
+    }
+} 
